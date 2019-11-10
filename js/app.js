d2902db191c96389d9d0f24f708c0d1c38bbd189
たむ <muta346@gmail.com>
2019-11-10 13:45:37 +0900

    dvopxjkdx

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(".js-toggle").on("click",function(){
+  $(this).siblings("dd").slideToggle();
+})
+$(function() {
+    $(document).snowfall({
+      minSize  : 5,    // 雪の最小サイズ
+      maxSize  : 10,    // 雪の最大サイズ
+      minSpeed : 1,    // 雪の最低速度
+      maxSpeed : 8,    // 雪の最高速度
+      round    : true, // 雪の形を丸くする
+      shadow   : true, // 雪に影をつける
+      flakeColor : "red", // 雪の色を指定
+    });
+  })
